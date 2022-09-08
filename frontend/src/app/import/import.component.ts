d5ec16e34dacfe7d273b977b4de59f71f030d361
Dominykas Zobakas <dzobakas@gmail.com>
2022-09-08 09:32:54 +0200

    Basic file upload frontend done

--- frontend/src/app/import/import.component.ts
+++ frontend/src/app/import/import.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http'
 
 @Component({
   selector: 'app-import',
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./import.component.css']
 })
 export class ImportComponent implements OnInit {
+  fileName ='';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+    onFileSelected(event:any) {
+
+        const file:File = event.target.files[0];
+
+        if (file) {
+
+            this.fileName = file.name;
+
+            const formData = new FormData();
+
+            formData.append("thumbnail", file);
+
+            const upload$ = this.http.post("/api/thumbnail-upload", formData);
+
+            upload$.subscribe();
+        }
+    }
 
   ngOnInit(): void {
   }
