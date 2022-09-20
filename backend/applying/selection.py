71b5738b2633e4ef3d85b2968ce7893a05ce5160
Dominykas Zobakas <dzobakas@gmail.com>
2022-09-20 11:32:26 +0200

    selection function works as intended

--- backend/applying/selection.py
+++ backend/applying/selection.py
@@ -1,12 +1,8 @@
 import pandas as pd
 import re
 class Selection():
-    def split(self, scope:str, sep:str):
-        return scope.split(sep)
 
     def compare_paths(self, path1, path2):
-        # print(path1)
-        # print(path2)
         if re.search(path2,path1):
             return True
         else:
@@ -15,8 +11,8 @@ class Selection():
     # "*" means any deepness of scope is allowed 
     def selection_function(self,regex :str, df: pd.DataFrame ,scope_column: str, scope_sep = "/"):
         scope = df[scope_column]
-        regex = re.sub("\?"+ scope_sep,'(\\\w+'+scope_sep+')?',regex)
-        regex = re.sub("\*" + scope_sep,'(\\\w+'+scope_sep+')*',regex)
+        regex = re.sub("\?"+ scope_sep,'((\\\w+|\\\s)+'+scope_sep+')?',regex)
+        regex = re.sub("\*" + scope_sep,'(.*'+scope_sep+')*',regex)
 
         paths = scope.apply(self.compare_paths, path2=(regex))
         paths = paths[paths] #Only where values True i.e. paths match
