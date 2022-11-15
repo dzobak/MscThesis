7c45ee3daeff5300f82d8edf08e58165d740d1bb
Eduardo Goulart Rocha <eduardo.grocha@hotmail.com>
2022-11-15 12:56:54 +0100

    Update functions_script.py
    Hi Domynikas, 
    
    I took a look into your code and there are quite a few problems with it:
    
    1- please try to follow common code conventions in Python (namely, PEP8). The IDE has good support for it and it will make your code easier to manage (also for you). Make sure that you watch (and understand) this two talks https://www.youtube.com/watch?v=anrOzOapJ2E , https://www.youtube.com/watch?v=wf-BqAjZb8M
    
    2- You should consider splitting your file into multiple files consisting of the different steps of your code (plus one file for the utils)
    
    3- As discussed before, the steps should be connected. The input is an OCEL, the output is another OCEL. These generated OCELs should also be "centrally managed". So you can loop over the steps multiple times (something like what ProM does)
    
    4- My feeling is that currently, the planing and execution of an operation are mixed. This is fine, but bear in mind that it will make it harder to develop more complex functions... You should aim for a two steps approach: The first step checks for constraints and builds an execution configuration (example, you can only call TRUNCATE if the column is of type scope). The second step executes the operation as it was configured in the first step
    
    5- I'll wait for the changes discussed in our past meeting

--- functions_script.py
+++ functions_script.py
@@ -1,124 +1,137 @@
-import pm4py 
-from pm4py.objects.ocel.importer import jsonocel
+# Python imports
+import os
 import re
-import pandas as pd
-import scipy
+from pathlib import Path
 
-#For aggregation and relabelling
+# Library imports
+import pandas as pd
+import pm4py
+
+
 def get_scope_tuple(scope):
+    # For aggregation and relabelling
     return tuple(scope.rsplit('/'))
 
-#For aggregation, checks if all the values provided in the series are the same
-def is_unique(s:pd.Series):
+
+def is_unique(s: pd.Series):
+    # For aggregation, checks if all the values provided in the series are the same
     a = s.to_numpy()
     return (a[0] == a).all()
 
-def truncate_lvl(scope_value,level):
+
+def truncate_lvl(scope_value, level):
     scope_tuple = get_scope_tuple(scope_value)
-    truncated_scope = '/'.join(scope_tuple[:level+1])
+    truncated_scope = '/'.join(scope_tuple[:level + 1])
     return truncated_scope
+
 
 def truncate(series):
-    first_scope = series.iloc[0] #After truncating, all scopes will be the same, so a representative is picked
-    
+    # After truncating, all scopes will be the same, so a representative is picked
+    first_scope = series.iloc[0]
+
     tuple_series = series.apply(get_scope_tuple)
     scope_df = pd.DataFrame(tuple_series.to_list())
-    
-    scope_equality_lvl =  -1 #holds the value until which scope level the scope should be truncated
+
+    # Holds the value until which scope level the scope should be truncated
+    scope_equality_lvl = -1
     for i in range(len(scope_df.columns)):
         if not is_unique(scope_df[i]):
-            scope_equality_lvl = i-1
+            scope_equality_lvl = i - 1
             break
-        elif i == len(scope_df.columns)-1:
+        elif i == len(scope_df.columns) - 1:
             scope_equality_lvl = i
-    
+
     truncated_scope = truncate_lvl(first_scope, scope_equality_lvl)
-            
+
     return truncated_scope
 
-#For Aggregation
-def dtype_to_func(col,dtype):
+
+def dtype_to_func(col, dtype):
+    # For Aggregation
     if re.search(r"scope", col, re.IGNORECASE):
         return truncate
-    elif dtype == type(""):
+    elif isinstance(dtype, str):
         return 'mode'
-    elif dtype == type(3) or dtype == type(3.0):
+    elif isinstance(dtype, int) or isinstance(dtype, float):
         return 'sum'
-    
+
 
 def aggregation(log, **kwargs):
-    
     col_func_map = {}
-    if  kwargs["evt_or_obj"] == "e":
+    is_event = kwargs["evt_or_obj"] == "e"
+    if is_event:
         special_columns = {
             "id_column": "ocel:eid",
             "ts_column": "ocel:timestamp",
             "act_column": "ocel:activity",
             "sc_column": kwargs["scope_column"]
-            }
+        }
 
-        log.events[special_columns["id_column"]] = log.events[special_columns["id_column"]].astype(float) 
+        log.events[special_columns["id_column"]] = log.events[special_columns["id_column"]].astype(
+            float)
         log.events["Scope1"] = log.events[special_columns["sc_column"]]
-        
+
         print("Scope examples: ")
         for i in range(5):
-            print(log.events[special_columns["sc_column"]][i*5])
-        
-        sc_lvl = int(input("Select the scope level: ")) 
+            print(log.events[special_columns["sc_column"]][i * 5])
 
-        
+        sc_lvl = int(input("Select the scope level: "))
+
         for col in log.events.columns:
             if col not in special_columns.values():
-                col_func_map[col] = dtype_to_func(col,type(log.events[col][0]))
-            
-            
+                col_func_map[col] = dtype_to_func(col, type(log.events[col][0]))
+
         col_func_map[special_columns["id_column"]] = "min"
-        col_func_map[special_columns["ts_column"]] = ['min','max']
-        col_func_map[special_columns["act_column"]] = lambda x: pd.Series.mode(x)[0]
+        col_func_map[special_columns["ts_column"]] = ['min', 'max']
+        col_func_map[special_columns["act_column"]] = lambda val: pd.Series.mode(val)[0]
 
-        log.events[special_columns["sc_column"]] = log.events[special_columns["sc_column"]].apply(truncate_lvl, level=sc_lvl)
-        
+        log.events[special_columns["sc_column"]] = log.events[special_columns["sc_column"]].apply(
+            truncate_lvl, level=sc_lvl)
+
         agg_log = log.events.groupby([special_columns["sc_column"],
-            pd.Grouper(key=special_columns["ts_column"], freq='20h')],
-            as_index=False).agg(col_func_map)
-
+                                      pd.Grouper(key=special_columns["ts_column"], freq='20h')],
+                                     as_index=False).agg(col_func_map)
 
         new_columns = []
         for x in agg_log.columns:
-            if x == (special_columns["ts_column"],'min'):
+            if x == (special_columns["ts_column"], 'min'):
                 new_columns.append(special_columns["ts_column"] + ':start')
             else:
                 new_columns.append(x[0])
-        agg_log.columns = new_columns   
+        agg_log.columns = new_columns
         agg_log.sort_values(special_columns["id_column"], inplace=True, ignore_index=True)
-        
-    print(agg_log)
-    print(agg_log.columns)
+
+        print(agg_log)
+        print(agg_log.columns)
+    else:
+        raise NotImplementedError("TODO implement aggregation for objects")
 
 
-#________________________SELECTION FUNCTION___________________________    
-    
+# ________________________SELECTION FUNCTION___________________________
 def compare_paths(path1, path2):
-        if re.search(path2,path1):
-            return True
-        else:
-            return False
+    if re.search(path2, path1):
+        return True
+    else:
+        return False
 
-# "*" means any deepness of scope is allowed 
-def selection_function(df: pd.DataFrame , scope_sep = "/", **kwargs):
+
+def selection_function(df: pd.DataFrame, scope_sep="/", **kwargs):
+    # "*" means any deepness of scope is allowed
     scope = df[kwargs["scope_column"]]
-    regex = re.sub("\?"+ scope_sep,'((\\\w+|\\\s)+'+scope_sep+')?',kwargs["regex"])
-    regex = re.sub("\*" + scope_sep,'(.*'+scope_sep+')*',regex)
+    regex = re.sub("\?" + scope_sep, '((\\\w+|\\\s)+' + scope_sep + ')?', kwargs["regex"])
+    regex = re.sub("\*" + scope_sep, '(.*' + scope_sep + ')*', regex)
     print(regex)
-    paths = scope.apply(compare_paths, path2=(regex))
-    paths = paths[paths] #Only where values True i.e. paths match
+    paths = scope.apply(compare_paths, path2=regex)
+    paths = paths[paths]  # Only where values True i.e. paths match
     return df.loc[paths.index]
 
+
 def execute_selection(log, **kwargs):
-    kwargs["regex"] = str(input("Specify regex: ")) 
-    if kwargs["evt_or_obj"] == 'e':
+    kwargs["regex"] = str(input("Specify regex: "))
+    is_event = kwargs["evt_or_obj"] == "e"
+    if is_event:
         df = selection_function(log.events, **kwargs)
-    elif kwargs["evt_or_obj"] == 'o':
+    else:
         grouped = log.objects.groupby(log.objects[kwargs["object_column"]])
         df = grouped.get_group(kwargs["object_type"])
         df = selection_function(df, **kwargs)
@@ -128,64 +141,69 @@ def execute_selection(log, **kwargs):
         #     if name == kwargs["object_type"]:
         #         df2 = selection_function(df2, **kwargs)
         #     df = pd.concat([df,df2])
-    
-    print(df) 
-
-
-#_____________________________Relabeling________________________________
-def get_scope_at_level(scope, lvl, sep="/"):
-    split = tuple(scope.rsplit(sep))
-    return split[min(len(split)-1,lvl)]
-
-def relabel_function(df: pd.DataFrame, column:str, **kwargs):
-    print("Scope examples: ")
-    for i in range(5):
-        print(df[kwargs["scope_column"]][i*5 % len(df)])
-    sc_lvl = int(input("Select the scope level: ")) 
-    df[column] = df[kwargs["scope_column"]].apply(get_scope_at_level, lvl = sc_lvl)
-    return df
-
-
-    
-
-def execute_relabel(log, **kwargs):
-    
-    if kwargs["evt_or_obj"] == 'e':
-        df = relabel_function(log.events, column="ocel:activity", **kwargs)
-    elif kwargs["evt_or_obj"] == 'o':
-       df = relabel_function(log.objects, column=kwargs["object_column"], **kwargs)
-    
     print(df)
 
 
-#_____________________________START OF SCRIPT___________________________________________________
-
-# filepath = str(input("Filepath: "))
-filepath = "/Users/dzoba/Studies/MasterThesis/Preparation/toy_log2.jsonocel"
-method =  str(input("Selection function (s), aggregation (a) or relabelling (r): "))
-if method not in ["s","a","r","test"]:
-    print("wrong method")
-else:
-    log = jsonocel.importer.apply(filepath)
-    kwargs = {}
-    kwargs["evt_or_obj"] = str(input("Should the action be performed on events (e) or objects (o)?: "))
-    if kwargs["evt_or_obj"] == 'e':
-        print(log.events.columns)
-    elif kwargs["evt_or_obj"] == 'o':
-        print(log.objects.columns)
-        # object_column = str(input("Select the column specifyng the object type: "))
-        kwargs["object_column"] = "ocel:type"
-        print(log.objects[log.object_type_column].value_counts().to_dict())
-        kwargs["object_type"] = str(input("Select the object type: "))
-    kwargs["scope_column"] = str(input("Select scope column: ")) 
+# _____________________________Relabeling________________________________
+def get_scope_at_level(scope, lvl, sep="/"):
+    split = tuple(scope.rsplit(sep))
+    return split[min(len(split) - 1, lvl)]
 
 
-if method == "s":
-    execute_selection(log, **kwargs)
-elif method == "a":
-    aggregation(log, **kwargs)
-elif method == 'r':
-    execute_relabel(log, **kwargs)
-elif method =="test": 
-    print(type(log.events["scope"]))
-    print(truncate(log.events["scope"]))
+def relabel_function(df: pd.DataFrame, column: str, **kwargs):
+    print("Scope examples: ")
+    for i in range(5):
+        print(df[kwargs["scope_column"]][i * 5 % len(df)])
+    sc_lvl = int(input("Select the scope level: "))
+    df[column] = df[kwargs["scope_column"]].apply(get_scope_at_level, lvl=sc_lvl)
+    return df
+
+
+def execute_relabel(log, **kwargs):
+    is_event = kwargs["evt_or_obj"] == "e"
+    relabel_column = "ocel:activity" if is_event else kwargs["object_column"]
+    df = relabel_function(log.objects, column=relabel_column, **kwargs)
+    print(df)
+
+
+def read_eventlog():
+    base_path = Path(os.getcwd())
+    file_path = base_path / "backend/event_logs/toy_log2.jsonocel"
+    return pm4py.read_ocel(str(file_path))
+
+
+# _____________________________START OF SCRIPT___________________________________________________
+
+def main():
+    log = read_eventlog()
+    method = str(input("Selection function (s), aggregation (a) or relabelling (r): "))
+
+    if method not in ["s", "a", "r", "test"]:
+        print("wrong method")
+    else:
+        kwargs = {
+            "evt_or_obj": str(
+                input("Should the action be performed on events (e) or objects (o)?: "))
+        }
+        if kwargs["evt_or_obj"] == 'e':
+            print(log.events.columns)
+        elif kwargs["evt_or_obj"] == 'o':
+            print(log.objects.columns)
+            # object_column = str(input("Select the column specifying the object type: "))
+            kwargs["object_column"] = "ocel:type"
+            print(log.objects[log.object_type_column].value_counts().to_dict())
+            kwargs["object_type"] = str(input("Select the object type: "))
+        kwargs["scope_column"] = str(input("Select scope column: "))
+
+        if method == "s":
+            execute_selection(log, **kwargs)
+        elif method == "a":
+            aggregation(log, **kwargs)
+        elif method == 'r':
+            execute_relabel(log, **kwargs)
+        elif method == "test":
+            print(type(log.events["scope"]))
+            print(truncate(log.events["scope"]))
+
+if __name__ == '__main__':
+    main()
